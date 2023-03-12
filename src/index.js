9c6fddeb9681e1351a6d2145901185294b80d76a
Koumi <alphaoumarbarry8876@gmail.com>
2023-03-12 21:36:52 +0000

    Deleted all the content of my components

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import "popper.js/dist/umd/popper"
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
-import "font-awesome/css/font-awesome.css"
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
