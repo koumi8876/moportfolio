c7057a6adc8dd6ed582b8a53445b46c62696d319
Koumi <alphaoumarbarry8876@gmail.com>
2023-03-12 20:20:40 +0000

    Installed Boostrap,awesome and jquery files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import "popper.js/dist/umd/popper"
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import "font-awesome/css/font-awesome.css"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
